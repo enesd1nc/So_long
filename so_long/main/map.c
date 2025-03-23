2b44e25b4fc5068817ca834142fe5a868b2013e5
Enes Dinç <mehmetenes2004.dinc@gmail.com>
2025-03-23 13:33:21 +0300

    42

---
+++ so_long/main/map.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdinc <mdinc@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 14:07:17 by mdinc             #+#    #+#             */
+/*   Updated: 2025/03/11 14:07:17 by mdinc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "ft_printf.h"
+
+int	handle_collectible(t_data *data, int x, int y)
+{
+	if (data->map[y][x] == 'C')
+	{
+		data->collectibles--;
+		ft_printf("collectible: %d\n", data->collectibles);
+		return (1);
+	}
+	return (0);
+}
+
+int	setup_map(t_data *data, char *map_path)
+{
+	data->map = read_map(map_path);
+	if (!data->map)
+		return (0);
+	if (!check_collectibles(data->map, get_map_width(data->map),
+			get_map_height(data->map)))
+	{
+		free_map(data->map);
+		data->map = NULL;
+		return (0);
+	}
+	check_valid_chars(data, get_map_height(data->map),
+		get_map_width(data->map));
+	data->collectibles = count_collectibles(data);
+	return (1);
+}
+
+int	is_hidden_file(const char *path)
+{
+	const char	*ptr = path;
+
+	while (*ptr)
+	{
+		if (*ptr == '/' && *(ptr + 1) == '.')
+			return (1);
+		if (ptr == path && *ptr == '.')
+			return (1);
+		ptr++;
+	}
+	return (0);
+}
